d5c31b4db7368bc3baba4f5b40cdca8fbe1d4974
Azhar <azharalbaaqi1@gmail.com>
2024-06-21 16:34:53 +0700

    add button and change section

--- js/scripts.js
+++ js/scripts.js
@@ -1,54 +1,74 @@
-/*!
-* Start Bootstrap - Agency v7.0.12 (https://startbootstrap.com/theme/agency)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
-*/
-//
-// Scripts
-// 
+    /*!
+    * Start Bootstrap - Agency v7.0.12 (https://startbootstrap.com/theme/agency)
+    * Copyright 2013-2023 Start Bootstrap
+    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
+    */
 
-window.addEventListener('DOMContentLoaded', event => {
+    // Scripts
 
-    // Navbar shrink function
-    var navbarShrink = function () {
-        const navbarCollapsible = document.body.querySelector('#mainNav');
-        if (!navbarCollapsible) {
-            return;
-        }
-        if (window.scrollY === 0) {
-            navbarCollapsible.classList.remove('navbar-shrink')
-        } else {
-            navbarCollapsible.classList.add('navbar-shrink')
-        }
-
-    };
-
-    // Shrink the navbar 
-    navbarShrink();
-
-    // Shrink the navbar when page is scrolled
-    document.addEventListener('scroll', navbarShrink);
-
-    //  Activate Bootstrap scrollspy on the main nav element
-    const mainNav = document.body.querySelector('#mainNav');
-    if (mainNav) {
-        new bootstrap.ScrollSpy(document.body, {
-            target: '#mainNav',
-            rootMargin: '0px 0px -40%',
-        });
-    };
-
-    // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector('.navbar-toggler');
-    const responsiveNavItems = [].slice.call(
-        document.querySelectorAll('#navbarResponsive .nav-link')
-    );
-    responsiveNavItems.map(function (responsiveNavItem) {
-        responsiveNavItem.addEventListener('click', () => {
-            if (window.getComputedStyle(navbarToggler).display !== 'none') {
-                navbarToggler.click();
+    window.addEventListener('DOMContentLoaded', event => {
+        // Navbar shrink function
+        var navbarShrink = function () {
+            const navbarCollapsible = document.body.querySelector('#mainNav');
+            if (!navbarCollapsible) {
+                return;
             }
+            if (window.scrollY === 0) {
+                navbarCollapsible.classList.remove('navbar-shrink')
+            } else {
+                navbarCollapsible.classList.add('navbar-shrink')
+            }
+        };
+
+        // Shrink the navbar 
+        navbarShrink();
+
+        // Shrink the navbar when page is scrolled
+        document.addEventListener('scroll', navbarShrink);
+
+        // Activate Bootstrap scrollspy on the main nav element
+        const mainNav = document.body.querySelector('#mainNav');
+        if (mainNav) {
+            new bootstrap.ScrollSpy(document.body, {
+                target: '#mainNav',
+                rootMargin: '0px 0px -40%',
+            });
+        };
+
+        // Collapse responsive navbar when toggler is visible
+        const navbarToggler = document.body.querySelector('.navbar-toggler');
+        const responsiveNavItems = [].slice.call(
+            document.querySelectorAll('#navbarResponsive .nav-link')
+        );
+        responsiveNavItems.map(function (responsiveNavItem) {
+            responsiveNavItem.addEventListener('click', () => {
+                if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                    navbarToggler.click();
+                }
+            });
+        });
+
+        // Fungsi untuk toggle teks pada kartu
+        function toggleCard(card) {
+            var content = card.querySelector('.card-text');
+            var toggleButton = card.querySelector('.toggle-button');
+
+            if (content.classList.contains('collapsed')) {
+                // Jika teks sedang singkat, tampilkan semua teks
+                content.classList.remove('collapsed');
+                toggleButton.textContent = 'Tutup';
+            } else {
+                // Jika teks sudah penuh, kembalikan ke singkat
+                content.classList.add('collapsed');
+                toggleButton.textContent = 'Lihat Selengkapnya';
+            }
+        }
+
+        // Event listener untuk memanggil fungsi toggle saat kartu diklik
+        var cards = document.querySelectorAll('.card');
+        cards.forEach(function (card) {
+            card.addEventListener('click', function () {
+                toggleCard(card);
+            });
         });
     });
-
-});
